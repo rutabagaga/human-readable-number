fd345ebd5f2574da1739303c8910695a7c6f7780
rutabagaga <komleva.darya@gmail.com>
2021-03-15 21:53:53 +0300

    fead: modified js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+	const  NumberOne = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	const  NumberTen = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+ 
+  if (number === 0){
+		return 'zero';
+  }
+	const hundreds = Math.floor(number / 100);
+	let ten = Math.floor((number - hundreds * 100) / 10);
+	let one = number - hundreds * 100 - ten * 10;
+	if (ten === 1) {
+	  one = number - hundreds * 100;
+	  ten = 0;
+	}
+	const hundredsText = hundreds ? NumberOne[hundreds] + ' hundred ' : '';
+	return (hundredsText + NumberTen[ten] + NumberOne[one]).trim()
+ }
